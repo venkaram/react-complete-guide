6045f9a7541a50c1deec566fa864b4b8538b584f
Venkataraman Krishnamurthy <venkataraman.k@candelalabs.io>
2019-01-23 12:40:26 +0530

    emoty

--- src/App.js
+++ src/App.js
@@ -1,27 +1,98 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      {id: '101', name: "Arun", age: 27},
+      {id: '102', name: "Hema", age: 30},
+      {id: '103', name: "Mano", age: 30},
+      {id: '104', name: "Venkat", age: 28}
+    ],
+    showPersons: false
+  }
+
+  nameChangeHandler = (event, id) => {
+    console.log(event);
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState({persons: persons});
+  }
+
+  togglePersonHandler = () => {
+    const doesShowPersons = this.state.showPersons;
+    this.setState({showPersons: !doesShowPersons});
+  }
+
+  deletePersonHandler = (personIndex) => {
+    const p = this.state.persons.slice();
+    p.splice(personIndex, 1);
+    this.setState({persons: p});
+  }
+
   render() {
+
+    const style = {
+      backgroundColor: 'green',
+      color: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if(this.state.showPersons){
+      persons = (
+        <div >
+          {this.state.persons.map((person, index) => {
+            return <Person 
+                      click={this.deletePersonHandler.bind(this, index)} 
+                      name={person.name} 
+                      age={person.age}
+                      key={person.id}
+                      changed={(event)=>this.nameChangeHandler(event, person.id)}></Person>
+          })}
+        </div>
+      );
+      style.backgroundColor = 'red';
+      
+    }
+
+    let classes = [];
+
+    if(this.state.persons.length <= 2){
+      classes.push('red');
+    } 
+    if(this.state.persons.length <= 1){
+      classes.push('bold');
+    }
+
+
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      
+        <div className="App">
+          <h1>Hi! I'm a react App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button 
+            style={style}
+            onClick={this.togglePersonHandler}>Toggle Persons</button>
+          {persons}
+        </div>
+      
     );
+    
   }
 }
 
